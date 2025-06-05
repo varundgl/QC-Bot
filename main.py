42a1954ffbd31624197386d66bc8562f89b420d6
varund2003 <dvarun2003@gmail.com>
2025-06-05 14:43:58 +0530

    upated logging logic

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    stream=open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
+    stream=sys.stdout
 )
 
 async def main():
